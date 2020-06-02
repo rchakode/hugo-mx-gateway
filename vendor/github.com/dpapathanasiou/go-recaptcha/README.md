7511603de409b1755092dd27359ac49e921a7dde
Chris Camel <camel.christophe@gmail.com>
2020-06-02 15:29:10 +0200

    add go-recaptcha dependency

---
+++ vendor/github.com/dpapathanasiou/go-recaptcha/README.md
@@ -0,0 +1,56 @@
+go-recaptcha
+============
+
+https://godoc.org/github.com/dpapathanasiou/go-recaptcha
+
+About
+-----
+
+This package handles [reCaptcha](https://www.google.com/recaptcha) (API versions [2](https://developers.google.com/recaptcha/intro) and [3](https://developers.google.com/recaptcha/docs/v3)) form submissions in [Go](http://golang.org/).
+
+Usage
+-----
+
+Install the package in your environment:
+
+```
+go get github.com/dpapathanasiou/go-recaptcha
+```
+
+To use it within your own code, import <tt>github.com/dpapathanasiou/go-recaptcha</tt> and call:
+
+```
+recaptcha.Init (recaptchaPrivateKey)
+```
+
+once, to set the reCaptcha private key for your domain, then:
+
+```
+recaptcha.Confirm (clientIpAddress, recaptchaResponse)
+```
+
+### [reCAPTCHA v2](https://developers.google.com/recaptcha/intro)
+For each reCaptcha form input you need to check, using the values obtained by reading the form's POST parameters (the <tt>recaptchaResponse</tt> in the above corresponds to the value of <tt>g-recaptcha-response</tt> sent by the reCaptcha server.)
+
+The recaptcha.Confirm() function returns either true (i.e., the captcha was completed correctly) or false, along with any errors (from the HTTP io read or the attempt to unmarshal the JSON reply).
+
+### [reCAPTCHA v3](https://developers.google.com/recaptcha/docs/v3)
+
+Version 3 works differently: instead of interrupting page visitors with a prompt, it runs in the background, computing a score.
+
+This repo has been updated to handle the [score and action in the response](recaptcha.go#L20), but the usage example is still in terms of version 2.
+
+Usage Example
+-------------
+
+Included with this repo is [example.go](example/example.go), a simple HTTP server which creates the reCaptcha form and tests the input.
+
+See the [instructions](example/README.md) for running the example for more details.
+
+## Donate
+
+If you find this work useful, please consider making a donation:
+
+<a href="bitcoin:14TM4ADKJbaGEi8Qr8dh4KfPBQmjTshkZ2">Bitcoin Donate</a> `14TM4ADKJbaGEi8Qr8dh4KfPBQmjTshkZ2`
+
+![QR code](https://bitref.com/qr.php?data=14TM4ADKJbaGEi8Qr8dh4KfPBQmjTshkZ2)
---
+++ vendor/github.com/dpapathanasiou/go-recaptcha/recaptcha.go
@@ -0,0 +1,71 @@
+// Package recaptcha handles reCaptcha (http://www.google.com/recaptcha) form submissions
+//
+// This package is designed to be called from within an HTTP server or web framework
+// which offers reCaptcha form inputs and requires them to be evaluated for correctness
+//
+// Edit the recaptchaPrivateKey constant before building and using
+package recaptcha
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+type RecaptchaResponse struct {
+	Success     bool      `json:"success"`
+	Score       float64   `json:"score"`
+	Action      string    `json:"action"`
+	ChallengeTS time.Time `json:"challenge_ts"`
+	Hostname    string    `json:"hostname"`
+	ErrorCodes  []string  `json:"error-codes"`
+}
+
+const recaptchaServerName = "https://www.google.com/recaptcha/api/siteverify"
+
+var recaptchaPrivateKey string
+
+// check uses the client ip address, the challenge code from the reCaptcha form,
+// and the client's response input to that challenge to determine whether or not
+// the client answered the reCaptcha input question correctly.
+// It returns a boolean value indicating whether or not the client answered correctly.
+func check(remoteip, response string) (r RecaptchaResponse, err error) {
+	resp, err := http.PostForm(recaptchaServerName,
+		url.Values{"secret": {recaptchaPrivateKey}, "remoteip": {remoteip}, "response": {response}})
+	if err != nil {
+		log.Printf("Post error: %s\n", err)
+		return
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("Read error: could not read body: %s", err)
+		return
+	}
+	err = json.Unmarshal(body, &r)
+	if err != nil {
+		log.Println("Read error: got invalid JSON: %s", err)
+		return
+	}
+	return
+}
+
+// Confirm is the public interface function.
+// It calls check, which the client ip address, the challenge code from the reCaptcha form,
+// and the client's response input to that challenge to determine whether or not
+// the client answered the reCaptcha input question correctly.
+// It returns a boolean value indicating whether or not the client answered correctly.
+func Confirm(remoteip, response string) (result bool, err error) {
+	resp, err := check(remoteip, response)
+	result = resp.Success
+	return
+}
+
+// Init allows the webserver or code evaluating the reCaptcha form input to set the
+// reCaptcha private key (string) value, which will be different for every domain.
+func Init(key string) {
+	recaptchaPrivateKey = key
+}
